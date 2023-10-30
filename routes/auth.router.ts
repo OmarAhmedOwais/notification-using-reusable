700c0ec587391103b70f31aca39cddd47007a4e7
X-it <afarg843@gmail.com>
2023-10-30 16:05:09 +0200

    google-v2

--- routes/auth.router.ts
+++ routes/auth.router.ts
@@ -73,7 +73,7 @@ authRoute.get(
 
     }),
   expressAsyncHandler(async (req: Request, res: Response) => {
-    res.redirect(`http://localhost:8008?${{ data: req.user }}`);
+    res.json({ data: req.user });
   })
 );
 
